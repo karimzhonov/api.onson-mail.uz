336386306744aa98ca43a35366f58092b66f7266
x.karimjonov <khtkarimzhonov@gmail.com>
2025-02-19 16:40:51 +0500

    test

--- api_onson_mail/cargo/api_customs/management/commands/api_customs_script.py
+++ api_onson_mail/cargo/api_customs/management/commands/api_customs_script.py
@@ -1,9 +1,10 @@
 from django.core.management import BaseCommand
 from cargo.api_customs.egov import ApiPushService
-
+from cargo.order.models import Order, _send_api_customs_data
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         api = ApiPushService()
-        print(api.get_statistics())
+        order = Order.objects.first()
+        _send_api_customs_data([order.id], ['test_api-onson-mail-cargo'])
 
--- api_onson_mail/cargo/api_customs/urls.py
+++ api_onson_mail/cargo/api_customs/urls.py
@@ -3,6 +3,6 @@ from .views import MQView, MQTestView, MQTestReceiveView
 
 urlpatterns = [
     path('mq/', MQView.as_view()),
-    path('mq/test/health-check/', MQTestView.as_view()),
-    path('mq/test/receive/', MQTestReceiveView.as_view()),
+    path('mq/test/health-check', MQTestView.as_view()),
+    path('mq/test/receive', MQTestReceiveView.as_view()),
 ]
