57854fd63953fa554538406f9ca2f15dd1738d29
x.karimjonov <khtkarimzhonov@gmail.com>
2025-03-10 12:38:21 +0500

    fixes

--- api_onson_mail/cargo/client/urls.py
+++ api_onson_mail/cargo/client/urls.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register('client', ClientViewSet, '')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("myid/auth/", myid_auth),
     path("myid/code/<code>/", myid_code),
     path('admin/', include("cargo.client.api_admin.urls")),
