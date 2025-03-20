a750a6289b173d1a5d6652df2f531959349708a4
x.karimjonov <khtkarimzhonov@gmail.com>
2025-03-20 15:57:50 +0500

    add client

--- api_onson_mail/cargo/client/views.py
+++ api_onson_mail/cargo/client/views.py
@@ -1,9 +1,21 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from ..models import CargoUser
 from .serializers import ClientSerializer
 from .models import Client
 
-class ClientViewSet(ReadOnlyModelViewSet):
+
+class ClientViewSet(ModelViewSet):
+    http_method_names = ['head', 'get', 'post']
     serializer_class = ClientSerializer
 
     def get_queryset(self):
         return self.request.user.cargouser.clients.all() if hasattr(self.request.user, 'cargouser') else Client.objects.none()
+
+    def post(self, request, *args, **kwargs):
+        pnfl = request.data.get("pnfl")
+        client = get_object_or_404(Client, pnfl=pnfl)
+        cargouser, _ = CargoUser.objects.get_or_create(user=request.user)
+        cargouser.clients.add(client)
+        return Response({}, 200)
